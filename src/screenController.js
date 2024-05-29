9a957f22fb76f5d56677aca29736a7085cf4d123
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-05-29 02:55:00 -0500

    modify boardSpaceDivs to include location data

--- src/screenController.js
+++ src/screenController.js
@@ -14,22 +14,26 @@ export default class ScreenController {
     const boardDiv = container.querySelector(".board");
     for (let row = 0; row < 11; row++) {
       for (let column = 0; column < 11; column++) {
+        const boardRow = 9 - row;
+        const boardColumn = column - 1;
         if (column === 0 && row === 10) {
           const cornerSpace = document.createElement("div");
           boardDiv.appendChild(cornerSpace);
         } else if (column === 0) {
           const rowLabel = document.createElement("div");
           rowLabel.className = "boardLabel";
-          rowLabel.textContent = 9 - row;
+          rowLabel.textContent = boardRow;
           boardDiv.appendChild(rowLabel);
         } else if (row === 10) {
           const columnLabel = document.createElement("div");
           columnLabel.className = "boardLabel";
-          columnLabel.textContent = column - 1;
+          columnLabel.textContent = boardColumn;
           boardDiv.appendChild(columnLabel);
         } else {
-          const space = board[(9 - row) * 10 + column - 1];
+          const space = board[boardRow * 10 + boardColumn];
           const spaceDiv = document.createElement("div");
+          spaceDiv.dataset.row = boardRow;
+          spaceDiv.dataset.column = boardColumn;
           spaceDiv.className = "boardSpace";
           if (space.ship) {
             spaceDiv.classList.add("ship");
