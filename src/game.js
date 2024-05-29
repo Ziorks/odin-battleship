be32874f58f988a297070ffc86220afdd3f3ee15
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-05-29 02:46:46 -0500

    Add playRound() to game and some minor changes

--- src/game.js
+++ src/game.js
@@ -1,12 +1,11 @@
 import Player from "./player";
 
 export default class Game {
-  #player1;
-  #player2;
+  #players = [];
 
   constructor(player1Name, player2Name) {
-    this.#player1 = new Player(player1Name);
-    this.#player2 = new Player(player2Name);
+    this.#players.push(new Player(player1Name));
+    this.#players.push(new Player(player2Name));
 
     const testShips = [
       { length: 2, location: [0, 0] },
@@ -17,19 +16,35 @@ export default class Game {
     ];
 
     testShips.forEach((ship) => {
-      this.#player1.placeShip(ship.length, ship.location);
-      this.#player2.placeShip(ship.length, ship.location);
+      this.#players[0].placeShip(ship.length, ship.location);
+      this.#players[1].placeShip(ship.length, ship.location);
     });
-
-    this.#player1.receiveAttack([0, 0]);
-    this.#player1.receiveAttack([1, 0]);
   }
 
   get player1Board() {
-    return this.#player1.board;
+    return this.#players[0].board;
   }
 
   get player2Board() {
-    return this.#player2.board;
+    return this.#players[1].board;
+  }
+
+  #togglePlayers() {
+    this.#players.reverse();
+  }
+
+  playRound(location) {
+    const attackResult = this.#players[1].receiveAttack(location);
+    if (attackResult !== null) {
+      if (this.#players[1].allShipsSunk()) {
+        return `Game Over.  ${this.#players[0].name} Wins!`;
+      }
+
+      const message = `${
+        this.#players[0].name
+      } attacked ${location} and it was a ${attackResult}.`;
+      this.#togglePlayers();
+      return message;
+    }
   }
 }
