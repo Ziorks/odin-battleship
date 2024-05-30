eb56af27ffd813aa0b17fada4b6abd5c42fffd7f
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-05-30 03:21:17 -0500

    Modified the way Game handles player logic

--- src/game.js
+++ src/game.js
@@ -1,11 +1,12 @@
-import Player from "./player";
-
 export default class Game {
-  #players = [];
+  #player1;
+  #player2;
+  #attackingPlayer;
 
-  constructor(player1Name, player2Name) {
-    this.#players.push(new Player(player1Name));
-    this.#players.push(new Player(player2Name));
+  constructor(player1, player2) {
+    this.#player1 = player1;
+    this.#player2 = player2;
+    this.#attackingPlayer = this.#player1;
 
     const testShips = [
       { length: 2, location: [0, 0] },
@@ -16,34 +17,44 @@ export default class Game {
     ];
 
     testShips.forEach((ship) => {
-      this.#players[0].placeShip(ship.length, ship.location);
-      this.#players[1].placeShip(ship.length, ship.location);
+      this.#player1.placeShip(ship.length, ship.location);
+      this.#player2.placeShip(ship.length, ship.location);
     });
   }
 
-  get player1Board() {
-    return this.#players[0].board;
+  get player1() {
+    return this.#player1;
   }
 
-  get player2Board() {
-    return this.#players[1].board;
+  get player2() {
+    return this.#player2;
   }
 
-  #togglePlayers() {
-    this.#players.reverse();
+  get attackingPlayer() {
+    return this.#attackingPlayer;
+  }
+
+  get receivingPlayer() {
+    return this.#attackingPlayer == this.#player1
+      ? this.#player2
+      : this.#player1;
+  }
+
+  #toggleAttackingPlayer() {
+    this.#attackingPlayer = this.receivingPlayer;
   }
 
   playRound(location) {
-    const attackResult = this.#players[1].receiveAttack(location);
+    const attackResult = this.receivingPlayer.receiveAttack(location);
     if (attackResult !== null) {
-      if (this.#players[1].allShipsSunk()) {
-        return `Game Over.  ${this.#players[0].name} Wins!`;
+      if (this.receivingPlayer.allShipsSunk()) {
+        return `Game Over.  ${this.#attackingPlayer.name} Wins!`;
       }
 
       const message = `${
-        this.#players[0].name
+        this.#attackingPlayer.name
       } attacked ${location} and it was a ${attackResult}.`;
-      this.#togglePlayers();
+      this.#toggleAttackingPlayer();
       return message;
     }
   }
